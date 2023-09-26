da3b875bd2fa70b04fd5ea8f3c1a6e2e917dac68
Konstantin Bazhenov <kot.bazhenov@ya.ru>
2023-09-26 22:09:19 +0300

    задача 3 выполнена

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -76,5 +76,15 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const funcResult = func(...arrOfArr[i]);
+
+    if (funcResult > maxWorkerResult) {
+      maxWorkerResult = funcResult;
+    }
+  }
+
+  return maxWorkerResult;
 }
