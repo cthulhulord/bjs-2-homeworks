45e4027d3e8d8f9f4d71a0e0f14b3713f4de350d
Konstantin Bazhenov <kot.bazhenov@ya.ru>
2023-09-26 20:30:35 +0300

    initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,19 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = arr[0];
+
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+  sum = arr.reduce((arrSum, currentSum) => arrSum + currentSum, 0);
+
+  let avg = +(sum / arr.length).toFixed(2)
 
   return { min: min, max: max, avg: avg };
 }
+
+
+
 
 function summElementsWorker(...arr) {
 
