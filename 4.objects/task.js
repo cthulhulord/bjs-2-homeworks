4b101369b1967fd19a6af33310c418c499b8f6a7
Konstantin Bazhenov <kot.bazhenov@ya.ru>
2023-10-26 15:30:56 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  	this.name = name;
+  	this.gender = gender;
+  	this.age = age;
+  	this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks != undefined) {
+  	this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks != undefined && this.marks.length != 0) {
+  	return this.marks.reduce((acc, curr) => acc + curr, 0) / this.marks.length;
+  } else {
+  	return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
+
+let student1 = new Student('Alex', 'male', 17);
+let student2 = new Student('Jane', 'female', 19);
+let student3 = new Student('Ann', 'female', 18); 
