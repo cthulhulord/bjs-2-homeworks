337e5b0a0c55de00208674be8ba27519788859c1
Konstantin Bazhenov <kot.bazhenov@ya.ru>
2023-10-29 22:03:34 +0300

    вторая задача

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,3 +16,20 @@ function validateCount(parseValue) {
 
 	}
 }
+
+class Triangle {
+	constructor(side1, side2, side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	get perimeter() {
+		return (this.side1 + this.side2 + this.side3);
+	}
+
+	get area() {
+		let p = this.perimeter;
+		return Math.sqrt(p * (p - this.side1) * (p - this.side2) * (p - this.side3));
+	}
+}
