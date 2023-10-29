02b264791ad5defce3e1790d376806434f3ef8ff
Konstantin Bazhenov <kot.bazhenov@ya.ru>
2023-10-29 21:32:24 +0300

    initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,18 @@
-﻿
+﻿function parseCount(parseValue) {
+			let parseResult = Number.parseFloat(parseValue);
+			if (Number.isNaN(parseResult)) {
+				throw new Error('Невалидное значение');
+			} else {
+				return parseResult;
+			}
+		}
+
+function validateCount(parseValue) {
+	try {
+		return parseCount(parseValue);
+		} catch (error){
+		return error;
+	} finally {
+
+	}
+}
